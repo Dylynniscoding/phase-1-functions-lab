aacb1fcb98670e46ddf184aca2e9a7561d36360b
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-11-01 12:05:52 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block)  {
+    if (block > 42) {
+        return block - 42;
+    } else {
+        return 42 - block;
+    }
+}
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+function distanceTravelledInFeet(start, end) {
+    if (start < end) {
+        return (end - start) * 264;
+    } else {
+        return (start - end) * 264;
+    }
+}
